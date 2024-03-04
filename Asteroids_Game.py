d9c79cac5ef4166b3c9f4175debbdfc1d6af0edf
CWalcott-GV <walcocor@mail.gvsu.edu>
2024-03-03 19:53:00 -0500

    Change space.png name, added more sprites, added GameObject (init, draw, move, collides_with)

--- Asteroids_Game.py
+++ Asteroids_Game.py
@@ -1,6 +1,7 @@
 import pygame
 
 from utils import load_sprite
+from pygame.math import Vector2
 
 class Asteroids_Game:
     def __init__(self):
@@ -29,3 +30,21 @@ class Asteroids_Game:
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
+
+class GameObject:
+    def __init__(self, position, sprite, velocity):
+        self.position = Vector2(position)
+        self.sprite = sprite
+        self.radius = sprite.get_width() / 2
+        self.velocity = Vector2(velocity)
+
+    def draw(self, surface):
+        blit_position = self.position - Vector2(self.radius)
+        surface.blit(self.sprite, blit_position)
+
+    def move(self):
+        self.position = self.position + self.velocity
+
+    def collides_with(self, other_obj):
+        distance = self.position.distance_to(other_obj.position)
+        return distance < self.radius + other_obj.radius
